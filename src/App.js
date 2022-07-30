dd7db334cea11ac0a3f3a60c39220783b01264df
Ashish Katiyar <katiyarashish24@gmail.com>
2022-07-30 18:49:38 +0530

    background img  done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Extension</div>;
+  // const fetchingData = async () => {
+  //   console.log("first");
+  //   try {
+  //     const response = await axios.get(
+  //       "https://api.unsplash.com/search/photos?page=1&query=nature&client_id=YKV8cJaG0G-U5QPFPAoUC7KXT7eAwUXenVylMmUAkxs"
+  //     );
+  //     console.log(response.data.results[0].urls.small);
+  //     // console.log(response.data.results);
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
+  return (
+    <div
+      className="App"
+      // style={{
+      //   backgroundImage: `url("https://source.unsplash.com/1920x1080/?wallpaper")`,
+      // }}
+    >
+      ashish
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Contexts/context.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const extensionContext = createContext();
+
+const extensionProvider = () => {};
